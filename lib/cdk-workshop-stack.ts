3375383b7267ba6c6d4aa3f03313fd6c7928a4c7
Alexandre Gressier <alexandre@gressier.dev>
2021-12-06 00:23:26 +0100

    Define the VPC

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,17 +1,19 @@
 import { Stack, StackProps } from "aws-cdk-lib"
 import { Construct } from "constructs"
-
-// import * as sqs from "aws-cdk-lib/aws-sqs"
+import { SubnetType, Vpc } from "aws-cdk-lib/aws-ec2"
 
 export class CdkWorkshopStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props)
 
-        // The code that defines your stack goes here
-
-        // example resource
-        // const queue = new sqs.Queue(this, "CdkWorkshopQueue", {
-        //     visibilityTimeout: cdk.Duration.seconds(300),
-        // })
+        const vpc = new Vpc(this, "VPC", {
+            subnetConfiguration: [
+                {
+                    cidrMask: 24,
+                    name: "Ingress",
+                    subnetType: SubnetType.PRIVATE_ISOLATED,
+                },
+            ],
+        })
     }
 }
